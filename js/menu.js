3173953236f5ba757c7a0c2551dfba22e8085c8b
Andrii_lishchynskyi <lav95g@gmail.com>
2026-03-26 10:40:23 -0400

    update

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,39 @@ const menu = document.querySelector('[data-menu]');
 const openBtn = document.querySelector('[data-menu-open]');
 const closeBtn = document.querySelector('[data-menu-close]');
 
-openBtn.addEventListener('click', () => {
-  menu.classList.add('is-open');
-});
+if (openBtn && closeBtn && menu) {
+  openBtn.addEventListener('click', () => {
+    menu.classList.add('is-open');
+  });
 
-closeBtn.addEventListener('click', () => {
-  menu.classList.remove('is-open');
-});
+  closeBtn.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+  });
+}
+
+const backdrop = document.querySelector('[data-modal]');
+const modalOpenBtn = document.querySelector('[data-modal-open]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+
+if (backdrop && modalOpenBtn && modalCloseBtn) {
+  const openModal = () => {
+    backdrop.classList.remove('hidden');
+    backdrop.classList.add('is-open');
+  };
+
+  const closeModal = () => {
+    backdrop.classList.remove('is-open');
+    backdrop.classList.add('hidden');
+  };
+
+  modalOpenBtn.addEventListener('click', openModal);
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  backdrop.addEventListener('click', event => {
+    if (event.target === backdrop) {
+      closeModal();
+    }
+  });
+}
 
 
